fc3d04cfadd925baf8a2b0620d013917f09f5769
Josh Come Ibad <joshcibad@gmail.com>
2020-11-14 22:15:20 -0800

    Update dropall.sql

--- dropall.sql
+++ dropall.sql
@@ -1,9 +1,9 @@
-DROP TABLE Professor;
+DROP TABLE Meeting;
+DROP TABLE Prerequisite;
+DROP TABLE Enrollment;
+DROP TABLE Minor;
+DROP TABLE Student;
 DROP TABLE Section;
 DROP TABLE Course;
 DROP TABLE Department;
-DROP TABLE Student;
-DROP TABLE Minor;
-DROP TABLE Enrollment;
-DROP TABLE Prerequisite;
-DROP TABLE Meeting;
+DROP TABLE Professor;
